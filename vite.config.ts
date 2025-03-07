a75397478c0f43a232c68117124f87bcd55c907a
Marcos Querino <mquerinojunior@gmail.com>
2025-03-07 00:44:07 -0300

    fix: Corrige a URL base.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/jr-cosmeticos/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
